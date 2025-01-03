fc1aab880349702b205edc69c06a963ddf5b9e16
Kavindu Mihiran <kavindumihiran2001@gmail.com>
2025-01-03 15:51:48 +0530

    Add Settings screen and update main entry point to display it

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:money_tracking_app/router/router.dart';
 import 'package:flutter/services.dart';
+import 'package:money_tracking_app/screens/settings_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,16 +21,17 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-       initialRoute: '/',
-      getPages: RouterClass().routes,
-      unknownRoute: GetPage(
-        name: '/notfound',
-        page: () => const Scaffold(
-          body: Center(
-            child: Text('This Page Not Found!'),
-          ),
-        ),
-      ),
+      home:  const SettingsScreen(),
+      //  initialRoute: '/',
+      // getPages: RouterClass().routes,
+      // unknownRoute: GetPage(
+      //   name: '/notfound',
+      //   page: () => const Scaffold(
+      //     body: Center(
+      //       child: Text('This Page Not Found!'),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
