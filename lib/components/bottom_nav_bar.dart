7f0c2f027a8b9c593b4c7d18a9095b57736e0b0e
sachintha990 <ict210156@std.uwu.ac.lk>
2025-01-03 13:51:24 +0530

    Refactor bottom navigation bar into a separate component

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.transparent,
+        unselectedItemColor: Colors.white60,
+        selectedItemColor: Colors.white,
+        showUnselectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: "Accounts",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      );
+    
+  }
+}
