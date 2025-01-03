aa8a3afd44e7ebcea0255689683deadb1b2a7daa
Udith Ayeshmantha <85647550+udithayeshmantha@users.noreply.github.com>
2025-01-04 02:40:26 +0530

    Add new transaction screen and integrate with navigation; refactor account dialog functionality

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:money_tracking_app/components/new_trannsaction.dart';
 import 'package:money_tracking_app/screens/Landing.dart';
 import 'package:money_tracking_app/screens/accounts.dart';
 import 'package:money_tracking_app/screens/categories.dart';
@@ -8,13 +9,30 @@ import 'package:money_tracking_app/screens/settings_screen.dart';
 
 class RouterClass {
   final List<GetPage> routes = [
-    GetPage( name: '/', page: () => Landing(),),
-    GetPage( name: '/login', page: () => Login(),),
-    GetPage( name: '/home',page: () =>  Home(),),
-    GetPage( name: '/accounts',page: () =>  AccountsScreen(),),
-    GetPage( name: '/categories',page: () =>  Categories(),),
-    GetPage( name: '/settings',page: () =>  SettingsScreen(),),
-    
-
+    GetPage(
+      name: '/',
+      page: () => Landing(),
+    ),
+    GetPage(
+      name: '/login',
+      page: () => Login(),
+    ),
+    GetPage(
+      name: '/home',
+      page: () => Home(),
+    ),
+    GetPage(
+      name: '/accounts',
+      page: () => AccountsScreen(),
+    ),
+    GetPage(
+      name: '/categories',
+      page: () => Categories(),
+    ),
+    GetPage(
+      name: '/settings',
+      page: () => SettingsScreen(),
+    ),
+    GetPage(name: '/newTransactionScreen', page: () => NewTransactionScreen())
   ];
 }
