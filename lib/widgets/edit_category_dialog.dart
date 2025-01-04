7b2b96ef0d299c752b369aca3062288e71a2e704
sachintha990 <ict210156@std.uwu.ac.lk>
2025-01-04 17:17:58 +0530

    Refactor categories screen to use stateful widget; add edit category dialog for improved category management

--- lib/widgets/edit_category_dialog.dart
+++ lib/widgets/edit_category_dialog.dart
@@ -1,137 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:money_tracking_app/components/bottom_nav_bar.dart';
-import 'package:money_tracking_app/widgets/edit_category_dialog.dart';
 
-class Categories extends StatefulWidget {
-  const Categories({Key? key}) : super(key: key);
+class EditCategoryDialog extends StatefulWidget {
+  final String name;
+  final String budget;
+  final IconData icon;
+  final Color color;
+  final Function(Map<String, dynamic>) onSave;
 
-  @override
-  _CategoriesState createState() => _CategoriesState();
-}
-
-class _CategoriesState extends State<Categories> {
-  final List<Map<String, Object>> categories = [
-    {"icon": Icons.home, "name": "Housing", "budget": "No budget", "color": Colors.blue},
-    {"icon": Icons.directions_car, "name": "Transportation", "budget": "No budget", "color": Colors.green},
-    {"icon": Icons.restaurant, "name": "Food", "budget": "No budget", "color": Colors.red},
-    {"icon": Icons.flash_on, "name": "Utilities", "budget": "No budget", "color": Colors.yellow},
-    {"icon": Icons.health_and_safety, "name": "Insurance", "budget": "No budget", "color": Colors.purple},
-    {"icon": Icons.medical_services, "name": "Medical & Healthcare", "budget": "No budget", "color": Colors.orange},
-    {"icon": Icons.savings, "name": "Saving, Investing, & Debt Payments", "budget": "No budget", "color": Colors.pink},
-    {"icon": Icons.shopping_cart, "name": "Personal Spending", "budget": "No budget", "color": Colors.brown},
-    {"icon": Icons.tv, "name": "Recreation & Entertainment", "budget": "No budget", "color": Colors.cyan},
-  ];
+  const EditCategoryDialog({
+    Key? key,
+    required this.name,
+    required this.budget,
+    required this.icon,
+    required this.color,
+    required this.onSave,
+  }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 20, 17, 24),
-      appBar: AppBar(
-        toolbarHeight: 75,
-        automaticallyImplyLeading: false, // Remove the back button
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        title: const Align(
-          alignment: Alignment.centerLeft,
-          child: Text(
-            "Categories",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 22,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: ListView.separated(
-        itemCount: categories.length,
-        itemBuilder: (context, index) {
-          final category = categories[index];
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundColor: (category['color'] as Color).withOpacity(0.1),
-              child: Icon(
-                category['icon'] as IconData,
-                color: category['color'] as Color,
-              ),
-            ),
-            title: Text(
-              category['name'] as String,
-              style: const TextStyle(color: Colors.white, fontSize: 18),
-            ),
-            subtitle: Text(
-              category['budget'] as String,
-              style: const TextStyle(color: Colors.white70),
-            ),
-            onTap: () {
-              _showEditCategoryDialog(
-                context,
-                category,
-                (updatedCategory) {
-                  setState(() {
-                    categories[index] = updatedCategory.cast<String, Object>();
-                  });
-                },
-              );
-            },
-          );
-        },
-        separatorBuilder: (context, index) => Divider(
-          indent: 50,
-          endIndent: 20,
-          color: Colors.grey.shade800,
-          thickness: 1,
-        ),
-      ),
-      bottomNavigationBar: BottomNavBar(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showAddCategoryDialog(context),
-        backgroundColor: const Color(0xff4b3887),
-        child: const Icon(
-          Icons.add,
-          color: Colors.white,
-        ),
-      ),
-    );
-  }
-
-
-  void _showEditCategoryDialog(BuildContext context, Map<String, Object> category, Function(Map<String, dynamic>) onSave) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return EditCategoryDialog(
-          name: category['name'] as String,
-          budget: category['budget'] as String,
-          icon: category['icon'] as IconData,
-          color: category['color'] as Color,
-          onSave: onSave,
-        );
-      },
-    );
-  }
+  _EditCategoryDialogState createState() => _EditCategoryDialogState();
 }
 
- void _showAddCategoryDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AddCategoryDialog();
-      },
-    );
-  }
+class _EditCategoryDialogState extends State<EditCategoryDialog> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController budgetController = TextEditingController();
+  late Color selectedColor;
+  late IconData selectedIcon;
 
-
-
-class AddCategoryDialog extends StatefulWidget {
   @override
-  _AddCategoryDialogState createState() => _AddCategoryDialogState();
-}
-
-class _AddCategoryDialogState extends State<AddCategoryDialog> {
-  Color selectedColor = Colors.primaries[0];
-  IconData selectedIcon = Icons.category;
+  void initState() {
+    super.initState();
+    nameController.text = widget.name;
+    budgetController.text = widget.budget;
+    selectedColor = widget.color;
+    selectedIcon = widget.icon;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -146,13 +48,14 @@ class _AddCategoryDialogState extends State<AddCategoryDialog> {
           mainAxisSize: MainAxisSize.min,
           children: [
             Text(
-              "New Category",
+              "Edit Category",
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
               ),
             ),
+            
             SizedBox(height: 16),
             Row(
               children: [
@@ -160,6 +63,7 @@ class _AddCategoryDialogState extends State<AddCategoryDialog> {
                 SizedBox(width: 8),
                 Expanded(
                   child: TextField(
+                    controller: nameController,
                     decoration: InputDecoration(
                       labelText: "Name",
                       labelStyle: TextStyle(color: Colors.white70),
@@ -177,6 +81,7 @@ class _AddCategoryDialogState extends State<AddCategoryDialog> {
             ),
             SizedBox(height: 16),
             TextField(
+              controller: budgetController,
               decoration: InputDecoration(
                 labelText: "Budget",
                 labelStyle: TextStyle(color: Colors.white70),
@@ -257,7 +162,14 @@ class _AddCategoryDialogState extends State<AddCategoryDialog> {
             SizedBox(height: 16),
             ElevatedButton(
               onPressed: () {
-                // Handle save action
+                widget.onSave({
+                  "name": nameController.text,
+                  "budget": budgetController.text.isEmpty
+                      ? "No budget"
+                      : budgetController.text,
+                  "icon": selectedIcon,
+                  "color": selectedColor,
+                });
                 Navigator.pop(context);
               },
               style: ElevatedButton.styleFrom(
